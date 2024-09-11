cedcf054f6c6ed017ce2cb1262e16abad82d9208
pilarsanpe <125973574+PilarSanPe@users.noreply.github.com>
2024-09-11 12:16:11 +0200

    update

--- FIELDTRIP/BIDS_convert.m
+++ FIELDTRIP/BIDS_convert.m
@@ -9,7 +9,8 @@
 % This file is part of FieldTrip, see http://www.fieldtriptoolbox.org
 % for the documentation and details.
 % -------------------------------------------------------------------------
-% This is a general example to export FIELDTRIP dataset.
+% This is a general example to export FIELDTRIP dataset. The data will be
+% output with the extension .vhdr.
 % First, you have to add this repository to path (https://github.com/fieldtrip/fieldtrip).
 % Modify this script for your own purpose.
 % -------------------------------------------------------------------------
@@ -19,46 +20,51 @@ clear all;
 
 %% Paths and raw data files (replace with your own)
 % Path of fieldtrip version
-addpath(genpath('C:\Users\Usuario\Documents\GitHub\fieldtrip')); % Path of the fieldtrip toolbox from where it takes the funtion to convert to BIDS format (do not add to Matlab path).
-datapathBIDS = dir('D:\DOCUMENTOS\MATLAB\EEG\PAULA\pruebafieldtrip\BIDS');
-datapathraw = 'D:\DOCUMENTOS\MATLAB\EEG\PAULA\pruebafieldtrip'; % In this folder shoud be participants folders.
 
-sub = {'001'}; %, '002', '003', '004', '005', '006', '007', '008', '009', '010'}; % Introduce all subjects that you want to convert in BIDS format.
-age = [25]; %96 nan 77  82  87  18 40  26  80]; % Introduce the age of your participants respectively.
-sex = {'f'}; %'f' 'f' 'f' 'm' 'm' 'm' 'm' 'm'}; % Introduce the sex of your participants respectively.
-%   ses = {'1', '2'}; % Change indicating your sessions. If you only have one session, you can omit this.
-%   run = []; If you do not have runs, you can omit this.
+addpath(genpath('C:\Users\Usuario\Documents\GitHub\fieldtrip')); % Path of the fieldtrip toolbox from where it takes the funtion to convert to BIDS format (do not add to Matlab path).
+datapathBIDS = dir('D:\DOCUMENTOS\MATLAB\EEG\BIDS');
+datapathraw = 'D:\DOCUMENTOS\MATLAB\EEG\sourcedata'; % In this folder shoud be participants folders.
+
+sub = {'001', '002', '003', '004', '005', '006', '007', '008', '009', '010', '011', '012'}; % Introduce all subjects that you want to convert in BIDS format.
+age = [32 40 19 nan 24 nan 45 nan 65 70 nan 18]; % Introduce the age of your participants respectively. If you do not have a participant's data write nan here.
+sex = {'m' '' '' 'm' 'f' 'f' '' 'm' 'f' 'm' 'm' 'f'}; % Introduce the sex of your participants respectively. If you do not have a participant's data write ''.
+% ses = {'1', '2'}; % Change indicating your sessions. If you only have one session, you can omit this.
+% run = [1 2]; % If you do not have runs, you can omit this.
 
 cfg.method    = 'convert'; % copy to copy data, convert to convert.
 cfg.datatype  = 'eeg';
 cfg.dataextension = '.vhdr'; % .set, .bdf, .eeg...
 
 %% Conversion with the data (replace with your own)
+
 for subindx = 1: numel(sub)
 %     for sesindx=1:numel(ses)
 %         for runindx=1:numel(run)
-            % specify the input file name, here we are using the same file for every subject:
+            % specify the input file name, here we are using the same file for every subject. It depends on the name of your data, so replace with your own:
             if subindx < 9
-                cfg.dataset   = [datapathraw filesep 'sub-00' int2str(subindx) filesep cfg.datatype filesep 'sub-00' int2str(subindx) cfg.dataextension];
+                cfg.dataset   = [datapathraw filesep 'sub-00' int2str(subindx) filesep cfg.datatype filesep 'sub-00' int2str(subindx) cfg.dataextension]; % example: sub-001 (folder), sub-001.vhdr (file)
                 cfg.outputfile   = datapathBIDS; % optional filename for the output data (path) (default is automatic)
             else
-                cfg.dataset   = [datapathraw filesep 'sub-0' int2str(subindx) filesep cfg.datatype filesep 'sub-0' int2str(subindx) cfg.dataextension];
+                cfg.dataset   = [datapathraw filesep 'sub-0' int2str(subindx) filesep cfg.datatype filesep 'sub-0' int2str(subindx) cfg.dataextension]; % example: sub-010 (folder), sub-010.vhdr (file)
                 cfg.outputfile   = datapathBIDS; % optional filename for the output data (path) (default is automatic)
             end
 
             %% Paths and data
             % Specify the output directory
+
             cfg.bidsroot  = 'BIDS';
             cfg.sub       = sub{subindx};
         
             %% Participant information for participants.tsv file
             % this is optional, you can also pass other pieces of info
+
             cfg.participants.age = age(subindx);
             cfg.participants.sex = sex{subindx};
         
             %% Event column description for xxx-events.json file (only one such file)
             % You do not need to define both trial type and value in this simple
             % example, but it is good to know that both exist.
+
             %   cfg.presentationfile        = string, optional filename for the presentation log file
             %   cfg.trigger.eventtype       = string (default = [])
             %   cfg.trigger.eventvalue      = string or number
@@ -69,6 +75,7 @@ for subindx = 1: numel(sub)
         
             %% Task information for xxxx-eeg.json file
             % General BIDS options that apply to all data types are
+
             cfg.InstitutionName             = 'CIMCYC';
             cfg.InstitutionalDepartmentName = 'Neurociencia cognitiva';
             cfg.InstitutionAddress          = 'Campus Cartuja, Granada';
@@ -79,6 +86,7 @@ for subindx = 1: numel(sub)
         
             %% General information for dataset_description.json file
             % If you specify cfg.bidsroot, this function will also write the dataset_description.json file. You can specify the following fields.
+         
             %   cfg.dataset_description.writesidecar        = string
             %   cfg.dataset_description.Name                = string
             %   cfg.dataset_description.BIDSVersion         = string
@@ -91,11 +99,13 @@ for subindx = 1: numel(sub)
             %   cfg.dataset_description.DatasetDOI          = string
         
             % Provide the mnemonic and long description of the task
+
             cfg.TaskName        = 'unknown'; % replace with your own
             cfg.TaskDescription = ''; % replace with your own
         
             % These are EEG specific
-            cfg.eeg.PowerLineFrequency = 60;   % since recorded in the USA
+            
+            cfg.eeg.PowerLineFrequency = 50;   % 60 in the USA, 50 Europe.
             cfg.eeg.EEGReference       = ''; % actually I do not know, but let's assume it was left mastoid
         
             data2bids(cfg);
--- FIELDTRIP/README.md
+++ FIELDTRIP/README.md
@@ -7,6 +7,8 @@ This script is used to export eeg data from different extensions to BIDS format.
 The presented file is generic for any experiment, so it should be adapted according to the needs of your experiment.
 This is indicated where it would be necessary.
 
+The data will be output with the extension .vhdr (compatible with Fieltrip).
+
 If you need to add more variables to BIDS files, you can find more information at http://www.fieldtriptoolbox.org.
 
 María Ruiz Romero and María del Pilar Sánchez Pedrajas (mariaruizromero@ugr.es, pilarsanpe@ugr.es)
