55b180a239b79a2fee908c691b7a8ab993e2d8b5
ruizromeerom <125973650+ruizromeerom@users.noreply.github.com>
2024-09-12 11:15:19 +0200

    code

--- CONVERT- BIDS- EEGLAB2FIELDTRIP/rename_bids_eeglab2fieldtrip.m
+++ CONVERT- BIDS- EEGLAB2FIELDTRIP/rename_bids_eeglab2fieldtrip.m
@@ -25,32 +25,34 @@ subjects = {
 % nsesions = 2; % replace with your own
 
 for subindx = 1 : length(subjects)
-   % for runindx=1 : nrun 
-        % for sesindx=1:nsesions
+%    for runindx=1 : nrun 
+%         for sesindx=1:nsesions
 
             % Set it to your data
             file = dir([datapath filesep subjects{subindx} filesep 'eeg' filesep ...
                 subjects{subindx} '_task-' taskname '_' datatype extension]);
             
 %             file = dir([datapath filesep subjects{subindx} filesep 'eeg' filesep ...
-%                 subjects{subindx} '_ses-0' sesindx '_task-' taskname '_run-' runindx datatype extension]); % If you have more than one session and run.
+%                 subjects{subindx} '_ses-0' int2str(sesindx) '_task-' taskname '_run-' int2str(runindx) datatype extension]); % If you have more than one session and run.
 %             file = dir([datapath filesep subjects{subindx} filesep 'eeg' filesep ...
-%                 subjects{subindx} '_task-' taskname '_run-' runindx datatype extension]); % If you have more than one run.
+%                 subjects{subindx} '_task-' taskname '_run-' int2str(runindx) datatype extension]); % If you have more than one run.
              
             cd(file.folder)
         
             EEG = pop_loadset(file.name);
+
+            data = strcat('data', int2str(runindx)) ;
         
-            data = eeglab2fieldtrip(EEG, 'raw', 'none');
+            data = eeglab2fieldtrip(EEG, 'preprocessing', 'none'); % 'preprocessing' for eeglab plugin / 'raw' fpr fieldtrip plugin
             
-            name = [subjects{sub} '_task-' taskname datatype 'mat'];
+            % Choose one depending on your data
+            name = [subjects{sub} '_task-' taskname datatype '.mat'];
+%             name = [subjects{sub} '_ses-0' int2str(sesindx) '_task-' taskname '_run-' int2str(runindx) datatype '.mat']; % If you have more than one session and run.
+%             name = [subjects{sub} '_task-' taskname '_run-' int2str(runindx) datatype '.mat']; % If you have more than one run.
             
             save(name, 'data');
         
-       % end
-
-   % end
-
-  
+%        end 
+%    end
 end
 
